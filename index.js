3ebe64b1da2289e20fc061076fe01a58138592e0
thebookkeepers-source <smazzz110@gmail.com>
2026-05-02 15:25:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const transporter = nodemailer.createTransport({
    Express app
 ══════════════════════════════════════════ */
 const app  = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 /* ── Middleware ── */
